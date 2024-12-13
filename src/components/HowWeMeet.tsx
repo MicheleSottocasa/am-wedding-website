c7923b55720995ca686bcd9a295590291d411523
MicheleSottocasa <michelesottocasa1@gmail.com>
2024-12-13 14:29:42 +0100

    feat: Creating new section HowWeMeet

---
+++ src/components/HowWeMeet.tsx
@@ -0,0 +1,22 @@
+import {Box} from "@mui/material";
+
+export default function HowWeMeet() {
+    return(
+        <Box className={"flex flex-col items-center justify-center w-full p-6"}>
+            <h1 className="font-eyesome text-center">Come ci siamo conosciuti</h1>
+
+            <Box className="flex flex-col md:flex-row items-center justify-center gap-12 md:gap-56 md:px-10 mt-6 w-full">
+                <Box className="text-center w-full max-w-xl">
+                    <h3 className="font-eyesome">Marti dice che...</h3>
+                    <p className="md:text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec aliquam orci, a dictum ligula. Vivamus sed semper dolor. In dui orci, scelerisque quis risus quis, placerat commodo massa. Proin eu nibh massa. Aliquam congue non tortor sed lobortis. Praesent lacinia orci eget augue sodales, fringilla ullamcorper risus malesuada. Maecenas quis nunc venenatis, pretium nulla ac, ultrices justo. Etiam nibh sapien, mollis id auctor id, malesuada mattis ligula.
+                        Donec imperdiet dui efficitur tincidunt condimentum. Aliquam quis erat porta, accumsan turpis sed, tincidunt dui. Cras nec tincidunt nibh. In urna velit, interdum vel urna vel, rutrum consequat augue. Etiam nec lacus finibus, fringilla arcu sed, blandit tortor. Pellentesque vel consequat arcu. In congue condimentum massa, porta finibus risus iaculis id. Sed interdum leo nec sem pulvinar, non venenatis purus facilisis. Duis pulvinar felis tortor, non consectetur ex fermentum rutrum. Integer ante sem, cursus non ex a, malesuada viverra massa. Vestibulum tincidunt mollis hendrerit.</p>
+                </Box>
+                <Box className="text-center w-full max-w-xl">
+                    <h3 className="font-eyesome">Marti dice che...</h3>
+                    <p className="md:text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec aliquam orci, a dictum ligula. Vivamus sed semper dolor. In dui orci, scelerisque quis risus quis, placerat commodo massa. Proin eu nibh massa. Aliquam congue non tortor sed lobortis. Praesent lacinia orci eget augue sodales, fringilla ullamcorper risus malesuada. Maecenas quis nunc venenatis, pretium nulla ac, ultrices justo. Etiam nibh sapien, mollis id auctor id, malesuada mattis ligula.
+                        Donec imperdiet dui efficitur tincidunt condimentum. Aliquam quis erat porta, accumsan turpis sed, tincidunt dui. Cras nec tincidunt nibh. In urna velit, interdum vel urna vel, rutrum consequat augue. Etiam nec lacus finibus, fringilla arcu sed, blandit tortor. Pellentesque vel consequat arcu. In congue condimentum massa, porta finibus risus iaculis id. Sed interdum leo nec sem pulvinar, non venenatis purus facilisis. Duis pulvinar felis tortor, non consectetur ex fermentum rutrum. Integer ante sem, cursus non ex a, malesuada viverra massa. Vestibulum tincidunt mollis hendrerit.</p>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import Hero from "@/components/Hero";
+import HowWeMeet from "@/components/HowWeMeet";
+import {Box} from "@mui/material";
 
 export default function Home() {
   return (
-      <Hero />
+      <Box>
+        <Hero />
+        <HowWeMeet />
+      </Box>
   );
 }
