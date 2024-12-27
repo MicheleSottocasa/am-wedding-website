858ddc06f9dd090a1cd44f67fafb567ed34919a7
MicheleSottocasa <michelesottocasa1@gmail.com>
2024-12-27 22:52:55 +0100

    chore: Updating howWeMeet copy

--- src/components/HowWeMeet.tsx
+++ src/components/HowWeMeet.tsx
@@ -8,15 +8,19 @@ export default function HowWeMeet() {
             <Box className="flex flex-col md:flex-row items-center justify-center gap-12 md:gap-56 md:px-10 mt-6 w-full">
                 <Box className="text-center w-full max-w-xl">
                     <h3 className="font-eyesome">Marti dice che...</h3>
-                    <p className="md:text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec aliquam orci, a dictum ligula. Vivamus sed semper dolor. In dui orci, scelerisque quis risus quis, placerat commodo massa. Proin eu nibh massa. Aliquam congue non tortor sed lobortis. Praesent lacinia orci eget augue sodales, fringilla ullamcorper risus malesuada. Maecenas quis nunc venenatis, pretium nulla ac, ultrices justo. Etiam nibh sapien, mollis id auctor id, malesuada mattis ligula.
-                        Donec imperdiet dui efficitur tincidunt condimentum. Aliquam quis erat porta, accumsan turpis sed, tincidunt dui. Cras nec tincidunt nibh. In urna velit, interdum vel urna vel, rutrum consequat augue. Etiam nec lacus finibus, fringilla arcu sed, blandit tortor. Pellentesque vel consequat arcu. In congue condimentum massa, porta finibus risus iaculis id. Sed interdum leo nec sem pulvinar, non venenatis purus facilisis. Duis pulvinar felis tortor, non consectetur ex fermentum rutrum. Integer ante sem, cursus non ex a, malesuada viverra massa. Vestibulum tincidunt mollis hendrerit.</p>
+                    <p className="md:text-xl">Un giorno, nel lontano 2015, causalmente Andrea compare tra le persone che potresti conoscere su Facebook.
+                        Decido quindi di inoltrargli la richiesta di amicizia e da lì qualche giorno dopo inizia a scrivermi e iniziamo a conoscerci…
+                    </p>
                 </Box>
                 <Box className="text-center w-full max-w-xl">
-                    <h3 className="font-eyesome">Marti dice che...</h3>
-                    <p className="md:text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec aliquam orci, a dictum ligula. Vivamus sed semper dolor. In dui orci, scelerisque quis risus quis, placerat commodo massa. Proin eu nibh massa. Aliquam congue non tortor sed lobortis. Praesent lacinia orci eget augue sodales, fringilla ullamcorper risus malesuada. Maecenas quis nunc venenatis, pretium nulla ac, ultrices justo. Etiam nibh sapien, mollis id auctor id, malesuada mattis ligula.
-                        Donec imperdiet dui efficitur tincidunt condimentum. Aliquam quis erat porta, accumsan turpis sed, tincidunt dui. Cras nec tincidunt nibh. In urna velit, interdum vel urna vel, rutrum consequat augue. Etiam nec lacus finibus, fringilla arcu sed, blandit tortor. Pellentesque vel consequat arcu. In congue condimentum massa, porta finibus risus iaculis id. Sed interdum leo nec sem pulvinar, non venenatis purus facilisis. Duis pulvinar felis tortor, non consectetur ex fermentum rutrum. Integer ante sem, cursus non ex a, malesuada viverra massa. Vestibulum tincidunt mollis hendrerit.</p>
+                    <h3 className="font-eyesome">Andre dice che...</h3>
+                    <p className="md:text-xl">Una sera la vedo in un locale perché sono andato a salutare un amico che era nella compagnia con cui era uscita lei,
+                        la vedo, mi colpisce, e il giorno dopo vedo la sua richiesta di amicizia su Facebook, quindi con la scusa che lei mi aveva chiesto l’amicizia io ho
+                        iniziato una conversazione…
+                    </p>
                 </Box>
             </Box>
+            <p className="md:text-xl text-center mt-10">Da quel momento in poi non abbiamo più smesso di sentirci e vederci!</p>
         </Box>
     )
 }
