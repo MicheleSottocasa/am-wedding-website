b6f07e6a9bf10aff5aaa1d734c1ba3c57cf496b9
MicheleSottocasa <michelesottocasa1@gmail.com>
2024-12-27 23:37:04 +0100

    chore: Updating images

--- src/components/HowWeMeet.tsx
+++ src/components/HowWeMeet.tsx
@@ -1,4 +1,7 @@
 import {Box} from "@mui/material";
+import Image from "next/image";
+import MartiImage from "@/assets/Marti.jpeg";
+import AndreImage from "@/assets/Andre.jpeg";
 
 export default function HowWeMeet() {
     return(
@@ -8,19 +11,42 @@ export default function HowWeMeet() {
             <Box className="flex flex-col md:flex-row items-center justify-center gap-12 md:gap-56 md:px-10 mt-6 w-full">
                 <Box className="text-center w-full max-w-xl">
                     <h3 className="font-eyesome">Marti dice che...</h3>
-                    <p className="md:text-xl">Un giorno, nel lontano 2015, causalmente Andrea compare tra le persone che potresti conoscere su Facebook.
-                        Decido quindi di inoltrargli la richiesta di amicizia e da lì qualche giorno dopo inizia a scrivermi e iniziamo a conoscerci…
+                    <div className="flex justify-center my-4">
+                        <Image
+                            src={MartiImage}
+                            alt="Martina"
+                            className="rounded-full w-48 h-48 object-cover"
+                            objectFit="cover"
+                            objectPosition="center"
+                        />
+                    </div>
+                    <p className="md:text-xl">Un giorno, nel lontano 2015, causalmente Andrea compare tra le persone che
+                        potresti conoscere su Facebook.
+                        Decido quindi di inoltrargli la richiesta di amicizia e da lì qualche giorno dopo inizia a
+                        scrivermi e iniziamo a conoscerci…
                     </p>
                 </Box>
                 <Box className="text-center w-full max-w-xl">
                     <h3 className="font-eyesome">Andre dice che...</h3>
-                    <p className="md:text-xl">Una sera la vedo in un locale perché sono andato a salutare un amico che era nella compagnia con cui era uscita lei,
-                        la vedo, mi colpisce, e il giorno dopo vedo la sua richiesta di amicizia su Facebook, quindi con la scusa che lei mi aveva chiesto l’amicizia io ho
+                    <div className="flex justify-center my-4">
+                        <Image
+                            src={AndreImage}
+                            alt="Martina"
+                            className="rounded-full w-48 h-48 object-cover"
+                            objectFit="cover"
+                            objectPosition="center"
+                        />
+                    </div>
+                    <p className="md:text-xl">Una sera la vedo in un locale perché sono andato a salutare un amico che
+                        era nella compagnia con cui era uscita lei,
+                        la vedo, mi colpisce, e il giorno dopo vedo la sua richiesta di amicizia su Facebook, quindi con
+                        la scusa che lei mi aveva chiesto l’amicizia io ho
                         iniziato una conversazione…
                     </p>
                 </Box>
             </Box>
-            <p className="md:text-xl text-center mt-10">Da quel momento in poi non abbiamo più smesso di sentirci e vederci!</p>
+            <p className="md:text-xl text-center mt-10">Da quel momento in poi non abbiamo più smesso di sentirci e
+                vederci!</p>
         </Box>
     )
 }
