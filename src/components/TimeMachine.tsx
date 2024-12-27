1a42d0469cc7a4eab9dc589226fbc817d56b397f
MicheleSottocasa <michelesottocasa1@gmail.com>
2024-12-28 00:15:51 +0100

    chore: Updating timeMachine style

--- src/components/TimeMachine.tsx
+++ src/components/TimeMachine.tsx
@@ -6,17 +6,17 @@ export default function TimeMachine(){
     return(
         <Box>
             <Box className="flex flex-col md:flex-row items-center justify-center m-5 gap-24">
-                <Image src={timeMachineImage} alt="time machine image"/>
-                <Box className="flex flex-col items-center justify-center gap- md:gap-10 md:text-4xl font-extralight font-eyesome text-center">
-                    <Box className="flex flex-col items-center justify-center gap-1 md:gap-8">
-                        <h3>Dal primo si</h3>
-                        <h3>11 Luglio 2015</h3>
+                <Image src={timeMachineImage} alt="time machine image" className="hidden md:block"/>
+                <Box className="flex flex-col items-center justify-center gap-6 md:gap-10 text-2xl md:text-4xl font-extralight  text-center">
+                    <Box className="flex flex-col items-center justify-center gap-4 md:gap-8">
+                        <p>Dal primo si</p>
+                        <p className="font-eyesome text-4xl font-semibold">11 Luglio 2015</p>
                     </Box>
-                    <h3>Alla tanto desiderata domanda</h3>
-                    <h3>24 Luglio 2024</h3>
+                    <p>Alla tanto desiderata domanda</p>
+                    <p className="font-eyesome text-4xl font-semibold">24 Luglio 2024</p>
                 </Box>
             </Box>
-            <h1 className="font-eyesome text-center">Al sì del per sempre</h1>
+            <h1 className="text-center font-light">Al sì del per sempre</h1>
             <h1 className="font-eyesome text-center">11 Luglio 2025</h1>
         </Box>
     )
